04f5a6f0dd96b21cbdbb761a7f10d26cb940fbae
gngeorgiev <georgin.georgiev@telerik.com>
2015-02-25 20:38:52 +0200

    Added ability to work with addresses without a hash(#), closes #8

--- polymer-route.js
+++ polymer-route.js
@@ -1,49 +1,49 @@
 (function () {
-    Polymer({
-        publish: {
-          params: {},
-          'default': false,
-          lazy: true,
-          reloadView: true,
-          redirect: ''
-        },
-        ready: function () {
-            this.elementName = this['element-name'];
-            this.loaded = false;
-            if (!this.lazy) {
-                this.loadView();
-            }
-        },
-        loadView: function (options) {
-            if (this.view) {
-                this.shadowRoot.removeChild(this.view);
-            }
+  Polymer({
+    publish: {
+      params: {},
+      'default': false,
+      lazy: true,
+      reloadView: true,
+      redirect: ''
+    },
+    ready: function () {
+      this.elementName = this['element-name'];
+      this.loaded = false;
+      if (!this.lazy) {
+        this.loadView();
+      }
+    },
+    loadView: function (options) {
+      if (this.view) {
+        this.shadowRoot.removeChild(this.view);
+      }
 
-            this.view = document.createElement(this.elementName);
-            this.setViewParams(options);
-            this.shadowRoot.appendChild(this.view);
-            this.loaded = true;
-        },
-        activate: function (options) {
-            if (this.lazy && (!this.loaded || this.reloadView || options.reload)) {
-                this.loadView(options);
-            } else {
-              this.setViewParams(options);
-            }
+      this.view = document.createElement(this.elementName);
+      this.setViewParams(options);
+      this.shadowRoot.appendChild(this.view);
+      this.loaded = true;
+    },
+    activate: function (options) {
+      if (this.lazy && (!this.loaded || this.reloadView || options.reload)) {
+        this.loadView(options);
+      } else {
+        this.setViewParams(options);
+      }
 
-            this.hidden = false;
-        },
-        setViewParams: function (options) {
-          options = options || {};
-          this.params = _.extend(this.params, options.params);
-          for (var i in this.params) {
-            if (this.params.hasOwnProperty(i)) {
-              this.view[i] = this.params[i];
-            }
-          }
-        },
-        hide: function () {
-            this.hidden = true;
+      this.hidden = false;
+    },
+    setViewParams: function (options) {
+      options = options || {};
+      this.params = _.extend(this.params, options.params);
+      for (var i in this.params) {
+        if (this.params.hasOwnProperty(i)) {
+          this.view[i] = this.params[i];
         }
-    });
+      }
+    },
+    hide: function () {
+      this.hidden = true;
+    }
+  });
 }());
--- polymer-router.js
+++ polymer-router.js
@@ -1,67 +1,79 @@
 (function () {
-    var hashstate = 'hashchange';
-    var load = 'load';
-    var polymerRoute = 'polymer-route';
+  var hashstate = 'hashchange';
+  var load = 'load';
+  var polymerRoute = 'polymer-route';
+  var hashSign = '#';
 
-    Polymer({
-        publish: {
-          routes: [],
-          routeElements: [],
-          defaultRouteElement: null,
-          currentRoute: null
-        },
-        onHashChange: function () {
-            this.go(location.hash);
-        },
-        go: function (hash, options) {
-            var route = this.findRoute(hash);
-            if (route) {
-              history.pushState(null, null, hash);
-              this.activateRoute(route, options);
-            }
-        },
-        findRoute: function (hash) {
-            var foundRoute = null;
-            [].forEach.call(this.routeElements, function (routeElement) {
-                routeElement.hide();
-                if (routeElement.default || foundRoute) {
-                      return;
-                }
+  Polymer({
+    publish: {
+      routes: [],
+      routeElements: [],
+      defaultRouteElement: null,
+      currentRoute: null
+    },
+    onHashChange: function () {
+      this.go(location.hash);
+    },
+    go: function (hash, options) {
+      hash = this.normalizeHash(hash);
+      var route = this.findRoute(hash);
+      if (route) {
+        history.pushState(null, null, hash);
+        this.activateRoute(route, options);
+      }
+    },
+    normalizeHash: function (hash) {
+      //TODO: support nested routes
 
-                var matcher = routeMatcher(routeElement.path);
-                var routeMatch = matcher.parse(hash);
-                if (routeMatch) {
-                  foundRoute = routeElement;
-                  routeElement.params = routeMatch;
-                }
-            });
+      if (hash.charAt(0) !== hashSign) {
+        return hashSign + hash;
+      }
 
-            return foundRoute || this.findRoute(this.defaultRouteElement.redirect);
-        },
-        activateRoute: function (route, options) {
-            route.activate(options);
-            this.currentRoute = route;
-        },
-        initRoutes: function () {
-            this.routeElements = this.shadowRoot.getElementsByTagName(polymerRoute);
-            this.defaultRouteElement = _.find(this.routeElements, function (route) {
-              return !!route.default;
-            });
-        },
-        attached: function () {
-            this.onHashChange = this.onHashChange.bind(this);
-            window.addEventListener(hashstate, this.onHashChange);
-
-            var loadHandler = function loadHandler () {
-                this.initRoutes();
-                this.go(location.hash);
-                window.removeEventListener(load, loadHandler);
-            }.bind(this);
-
-            window.addEventListener(load, loadHandler);
-        },
-        detached: function () {
-            window.removeEventListener(hashstate, this.onHashChange);
+      return hash;
+    },
+    findRoute: function (hash) {
+      var foundRoute = null;
+      [].forEach.call(this.routeElements, function (routeElement) {
+        routeElement.hide();
+        if (routeElement.default || foundRoute) {
+          return;
         }
-    });
+
+        var normalizedPath = this.normalizeHash(routeElement.path);
+        var matcher = routeMatcher(normalizedPath);
+        var routeMatch = matcher.parse(hash);
+        if (routeMatch) {
+          foundRoute = routeElement;
+          routeElement.params = routeMatch;
+        }
+      }.bind(this));
+
+      return foundRoute || this.findRoute(this.defaultRouteElement.redirect);
+    },
+    activateRoute: function (route, options) {
+      route.activate(options);
+      this.currentRoute = route;
+    },
+    initRoutes: function () {
+      this.routeElements = this.shadowRoot.getElementsByTagName(polymerRoute);
+      this.defaultRouteElement = _.find(this.routeElements, function (route) {
+        return !!route.default;
+      });
+    },
+    attached: function () {
+      this.onHashChange = this.onHashChange.bind(this);
+      window.addEventListener(hashstate, this.onHashChange);
+
+      var loadHandler = function loadHandler() {
+        this.initRoutes();
+        this.go(location.hash);
+        window.removeEventListener(load, loadHandler);
+      }.bind(this);
+
+      window.addEventListener(load, loadHandler);
+    },
+    detached: function () {
+      window.removeEventListener(hashstate, this.onHashChange);
+    }
+  });
 }());
